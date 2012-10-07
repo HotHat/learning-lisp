3c3dac90e1bb2edf753ed989f124c9b451d2d951
rob Arseneault <shoelessrob@gmail.com>
2012-10-07 22:45:27 +0000

    Had prematuraly commited the file as it was in half way state

--- cd.lisp
+++ cd.lisp
@@ -20,5 +20,7 @@
 
 (defun prompt-for-cd ()
   (make-cd
-   (prompt-raed "Title: ")   
-
+   (prompt-read "Title: ")   
+   (prompt-read "Artist: ")   
+   (or (parse-integer (prompt-read "Rating: ") :junk-allowed t) 0)   
+   (y-or-n-p "Ripped[y/n]?: ")))
